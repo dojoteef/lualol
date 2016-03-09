be53f7fb72ac060145667040a170997294708989
Nader Akoury <dojoteef@gmail.com>
2016-03-08 22:56:19 -0800

    Added the rest of the summoner endpoints and the tests to go along with them.

--- spec/summoner_spec.lua
+++ spec/summoner_spec.lua
@@ -189,5 +189,301 @@ describe('lol.summoner', function()
                 s3:revert()
             end)
         end)
+
+        insulate('getByIds', function()
+            local testSummoner
+            setup(function()
+                testSummoner = summoner(testApi)
+            end)
+
+            before_each(function()
+                testSummoner.api.cache:clearAll()
+            end)
+
+            insulate('summoner', function()
+                it('uses api get on cache miss', function()
+                    local s1 = spy.new(function() end)
+                    local s2 = stub(testSummoner.api, 'get',function() s1() end)
+                    testSummoner:getByIds({1,2}, nil, s1)
+
+                    assert.spy(s1).called(1)
+                    assert.stub(s2).called(1)
+
+                    local url = {
+                        path='/api/lol/${region}/v${version}/summoner/${summonerIds}${filter}',
+                        params={version=testSummoner.version,summonerIds='1,2',filter=''},
+                    }
+                    assert.stub(s2).called_with(testSummoner.api,match.same(url),match.is_function())
+                    s2:revert()
+                end)
+
+                local function dataFromRes(res)
+                    local data = {{}, res[2], res[3]}
+                    for _,v in pairs(res[1]) do
+                        data[1][v.name] = v
+                    end
+                    return data
+                end
+
+                it('caches api entries for 24 hours', function()
+                    local mockRes = { {}, 200, {}}
+                    mockRes[1]['1']={id=1,name='one'}
+                    mockRes[1]['2']={id=2,name='two'}
+
+                    local api = testSummoner.api
+                    local cache = api.cache
+
+                    local s1 = spy.new(function() end)
+                    local s2 = stub(cache, 'set')
+                    local s3 = stub(api, 'get', function(_,_,c) c(unpack(mockRes)) end)
+                    testSummoner:getByIds({1,2}, nil, s1)
+
+                    assert.spy(s1).called(1)
+                    assert.spy(s1).called_with(unpack(dataFromRes(mockRes)))
+
+                    assert.stub(s2).called(4)
+                    local cacheKey = {api='summoner',summonerName='one'}
+                    assert.stub(s2).called_with(cache,match.same(cacheKey),1)
+
+                    cacheKey = {api='summoner',summonerId=1}
+                    assert.stub(s2).called_with(cache,match.same(cacheKey),mockRes[1]['1'],24*60*60)
+
+                    cacheKey = {api='summoner',summonerName='two'}
+                    assert.stub(s2).called_with(cache,match.same(cacheKey),2)
+
+                    cacheKey = {api='summoner',summonerId=2}
+                    assert.stub(s2).called_with(cache,match.same(cacheKey),mockRes[1]['2'],24*60*60)
+                    s2:revert()
+                    s3:revert()
+                end)
+
+                it('will return previously cached entries', function()
+                    -- order cache entries back to front
+                    local cacheEntries = {{id=2,name='two'},{id=1,name='one'}}
+                    local mockRes = {{}, 200, {}}
+                    mockRes[1]['3']={id=3,name='three'}
+
+                    local api = testSummoner.api
+                    local cache = api.cache
+
+                    local s1 = spy.new(function() end)
+                    local s2 = stub(cache, 'get', function()
+                        -- table.remove remove last entry first
+                        return table.remove(cacheEntries)
+                    end)
+                    local s3 = stub(api, 'get', function(_,_,c) c(unpack(mockRes)) end)
+                    testSummoner:getByIds({1,2,3}, nil, s1) 
+
+                    assert.spy(s1).called(2) -- 1st for api, 2nd for cache
+                    assert.spy(s1).called_with(unpack(dataFromRes(mockRes)))
+                    assert.spy(s1).called_with(match.same({one={id=1,name='one'},two={id=2,name='two'}}))
+
+                    s2:revert()
+                    s3:revert()
+                end)
+
+                it('will page results', function()
+                    -- order cache entries back to front
+                    local cacheEntries = {{id=2,name='two'},{id=1,name='one'}}
+                    local api = testSummoner.api
+                    local cache = api.cache
+
+                    local mockRes1 = {{}, 200, {}}
+                    local ids = {1,2}
+                    for i = 3, 40 do
+                        ids[i] = i
+                        local name='summoner'..tostring(i)
+                        mockRes1[1][tostring(i)] = {id=i,name=name}
+                    end
+
+                    local mockRes2 = {{}, 200, {}}
+                    for i = 41,60 do
+                        ids[i] = i
+                        local name='summoner'..tostring(i)
+                        mockRes2[1][name] = {id=i,name=name}
+                    end
+
+                    local reslist = {mockRes2,mockRes1}
+
+                    local s1 = spy.new(function() end)
+                    local s2 = stub(cache, 'get', function()
+                        -- table.remove remove last entry first
+                        return table.remove(cacheEntries)
+                    end)
+                    local s3 = stub(api, 'get', function(_,_,c) c(unpack(table.remove(reslist))) end)
+                    testSummoner:getByIds(ids, nil, s1) 
+
+                    assert.spy(s1).called(3) -- 1st & 2nd for api, 3rd for cache
+                    assert.spy(s1).called_with(unpack(dataFromRes(mockRes1)))
+                    assert.spy(s1).called_with(unpack(dataFromRes(mockRes2)))
+                    assert.spy(s1).called_with(match.same({one={id=1,name='one'},two={id=2,name='two'}}))
+
+                    s2:revert()
+                    s3:revert()
+                end)
+            end)
+
+            insulate('name', function()
+                local filter = 'name'
+                it('uses api get on cache miss', function()
+                    local s1 = spy.new(function() end)
+                    local s2 = stub(testSummoner.api, 'get',function() s1() end)
+                    testSummoner:getByIds({1,2}, filter, s1)
+
+                    assert.spy(s1).called(1)
+                    assert.stub(s2).called(1)
+
+                    local url = {
+                        path='/api/lol/${region}/v${version}/summoner/${summonerIds}${filter}',
+                        params={version=testSummoner.version,summonerIds='1,2',filter='/'..filter},
+                    }
+                    assert.stub(s2).called_with(testSummoner.api,match.same(url),match.is_function())
+                    s2:revert()
+                end)
+
+                local function dataFromRes(res)
+                    local data = {{}, res[2], res[3]}
+                    for idstr,name in pairs(res[1]) do
+                        data[1][tonumber(idstr)] = name
+                    end
+                    return data
+                end
+
+                it('caches api entries for 24 hours', function()
+                    local mockRes = { {}, 200, {}}
+                    mockRes[1]['1']='one'
+                    mockRes[1]['2']='two'
+
+                    local api = testSummoner.api
+                    local cache = api.cache
+
+                    local s1 = spy.new(function() end)
+                    local s2 = stub(cache, 'set')
+                    local s3 = stub(api, 'get', function(_,_,c) c(unpack(mockRes)) end)
+                    testSummoner:getByIds({1,2}, filter, s1)
+
+                    assert.spy(s1).called(1)
+                    assert.spy(s1).called_with(unpack(dataFromRes(mockRes)))
+
+                    assert.stub(s2).called(2)
+                    local cacheKey = {api='summoner',summonerName='one'}
+                    assert.stub(s2).called_with(cache,match.same(cacheKey),1)
+
+                    cacheKey = {api='summoner',summonerName='two'}
+                    assert.stub(s2).called_with(cache,match.same(cacheKey),2)
+                    s2:revert()
+                    s3:revert()
+                end)
+
+                it('will return previously cached entries', function()
+                    -- order cache entries back to front
+                    local cacheEntries = {{id=2,name='two'},{id=1,name='one'}}
+                    local mockRes = {{}, 200, {}}
+                    mockRes[1]['3']='three'
+
+                    local api = testSummoner.api
+                    local cache = api.cache
+
+                    local s1 = spy.new(function() end)
+                    local s2 = stub(cache, 'get', function()
+                        -- table.remove remove last entry first
+                        return table.remove(cacheEntries)
+                    end)
+                    local s3 = stub(api, 'get', function(_,_,c) c(unpack(mockRes)) end)
+                    testSummoner:getByIds({1,2,3}, filter, s1) 
+
+                    assert.spy(s1).called(2) -- 1st for api, 2nd for cache
+                    assert.spy(s1).called_with(unpack(dataFromRes(mockRes)))
+                    assert.spy(s1).called_with(match.same({'one','two'}))
+
+                    s2:revert()
+                    s3:revert()
+                end)
+            end)
+
+            insulate('masteries and runes', function()
+                local filterTestFn = function(filter)
+                    it('uses api get on cache miss', function()
+                        local s1 = spy.new(function() end)
+                        local s2 = stub(testSummoner.api, 'get',function() s1() end)
+                        testSummoner:getByIds({1,2}, filter, s1)
+
+                        assert.spy(s1).called(1)
+                        assert.stub(s2).called(1)
+
+                        local url = {
+                            path='/api/lol/${region}/v${version}/summoner/${summonerIds}${filter}',
+                            params={version=testSummoner.version,summonerIds='1,2',filter='/'..filter},
+                        }
+                        assert.stub(s2).called_with(testSummoner.api,match.same(url),match.is_function())
+                        s2:revert()
+                    end)
+
+                    local function dataFromRes(res)
+                        local data = {{}, res[2], res[3]}
+                        for idstr,val in pairs(res[1]) do
+                            data[1][tonumber(idstr)] = val.pages
+                        end
+                        return data
+                    end
+
+                    it('caches api entries for 24 hours', function()
+                        local mockRes = { {}, 200, {}}
+                        mockRes[1]['1']={pages={}}
+                        mockRes[1]['2']={pages={}}
+
+                        local api = testSummoner.api
+                        local cache = api.cache
+
+                        local s1 = spy.new(function() end)
+                        local s2 = stub(cache, 'set')
+                        local s3 = stub(api, 'get', function(_,_,c) c(unpack(mockRes)) end)
+                        testSummoner:getByIds({1,2}, filter, s1)
+
+                        assert.spy(s1).called(1)
+                        assert.spy(s1).called_with(unpack(dataFromRes(mockRes)))
+
+                        assert.stub(s2).called(2)
+                        local cacheKey = {api='summoner',data=filter,summonerId=1}
+                        assert.stub(s2).called_with(cache,match.same(cacheKey),mockRes[1]['1'].pages,24*60*60)
+
+                        cacheKey = {api='summoner',data=filter,summonerId=2}
+                        assert.stub(s2).called_with(cache,match.same(cacheKey),mockRes[1]['2'].pages,24*60*60)
+                        s2:revert()
+                        s3:revert()
+                    end)
+
+                    it('will return previously cached entries', function()
+                        -- order cache entries back to front
+                        local page1 = {}
+                        local page2 = {}
+                        local cacheEntries = {page2,page1}
+                        local mockRes = {{}, 200, {}}
+                        mockRes[1]['3']={pages={}}
+
+                        local api = testSummoner.api
+                        local cache = api.cache
+
+                        local s1 = spy.new(function() end)
+                        local s2 = stub(cache, 'get', function()
+                            -- table.remove remove last entry first
+                            return table.remove(cacheEntries)
+                        end)
+                        local s3 = stub(api, 'get', function(_,_,c) c(unpack(mockRes)) end)
+                        testSummoner:getByIds({1,2,3}, filter, s1) 
+
+                        assert.spy(s1).called(2) -- 1st for api, 2nd for cache
+                        assert.spy(s1).called_with(unpack(dataFromRes(mockRes)))
+                        assert.spy(s1).called_with(match.same({page1,page2}))
+
+                        s2:revert()
+                        s3:revert()
+                    end)
+                end
+
+                filterTestFn('masteries')
+                filterTestFn('runes')
+            end)
+        end)
     end)
 end)
--- src/lol/summoner.lua
+++ src/lol/summoner.lua
@@ -29,12 +29,20 @@ function _summoner.new(apiObj)
     return setmetatable(obj, _summoner)
 end
 
+local function cacheKeyForName(summonerName)
+    return {api='summoner',summonerName=summonerName}
+end
+
 local function cacheKeyForId(summonerId)
     return {api='summoner',summonerId=summonerId}
 end
 
-local function cacheKeyForName(summonerName)
-    return {api='summoner',summonerName=summonerName}
+local function cacheKeyForMasteries(summonerId)
+    return {api='summoner',data='masteries',summonerId=summonerId}
+end
+
+local function cacheKeyForRunes(summonerId)
+    return {api='summoner',data='runes',summonerId=summonerId}
 end
 
 --- Given a Summoner name, put it into the League of Legends API standardized format
@@ -87,8 +95,8 @@ function _summoner:getByNames(names, callback)
         end
 
         if summoner then
-            cachedCount = cachedCount + 1
             cachedSummoners[summonerName] = summoner
+            cachedCount = cachedCount + 1
         else
             local nameString = url.params.summonerNames
             url.params.summonerNames = nameString and nameString..','..summonerName or summonerName
@@ -106,6 +114,119 @@ function _summoner:getByNames(names, callback)
 
     if cachedCount > 0 and callback then
         callback(cachedSummoners)
+    end
+end
+
+--- Get multiple Summoners from the League of Legends API given their Summoner ids
+-- @param ids - an array-like table with the list of Summoner ids to retreive
+-- @param filter - optional parameter denoting what type of information to retreive
+--   * valid values: 'name', 'masteries', 'runes'
+-- @param callback - a callback which receives the response from the API
+--   * NOTE: Since you may only retreive 40 summoners at a time the callback may
+--   * be called multiple times from a single call to summoner:getByIds
+function _summoner:getByIds(ids, filter, callback)
+    local cache = self.api.cache
+    local onResponse = function(res, code, headers)
+        local data = {}
+        for _,summoner in pairs(res) do
+            local summonerName = self.standardizeSummonerName(summoner.name)
+            cache:set(cacheKeyForName(summonerName),summoner.id)
+            cache:set(cacheKeyForId(summoner.id),summoner,24*60*60)
+
+            data[summonerName] = summoner
+        end
+
+        if callback then
+            callback(data, code, headers)
+        end
+    end
+
+    local cacheKeyForFilter = {
+        name=cacheKeyForId,
+        masteries=cacheKeyForMasteries,
+        runes=cacheKeyForRunes
+    }
+
+    local onFilterResponse = {
+        name=function(res, code, headers)
+            local data = {}
+            for idstr,name in pairs(res) do
+                local id = tonumber(idstr)
+                local summonerName = self.standardizeSummonerName(name)
+                cache:set(cacheKeyForName(summonerName),id)
+
+                data[id] = name
+            end
+
+            if callback then
+                callback(data, code, headers)
+            end
+        end,
+        masteries=function(res, code, headers)
+            local data = {}
+            for idstr,val in pairs(res) do
+                local id = tonumber(idstr)
+                cache:set(cacheKeyForMasteries(id),val.pages,24*60*60)
+
+                data[id] = val.pages
+            end
+
+            if callback then
+                callback(data, code, headers)
+            end
+        end,
+        runes=function(res, code, headers)
+            local data = {}
+            for idstr,val in pairs(res) do
+                local id = tonumber(idstr)
+                cache:set(cacheKeyForRunes(id),val.pages,24*60*60)
+
+                data[id] = val.pages
+            end
+
+            if callback then
+                callback(data, code, headers)
+            end
+        end
+    }
+
+    if filter then
+        utils.assert_arg(2,filter,'string',function() return onFilterResponse[filter] end,'not a valid api object')
+    end
+
+    local cachedCount = 0
+    local cachedData = {}
+    local maxNamesPerQuery = 40
+    local url = {
+        params={version=self.version,filter=filter and '/'..filter or ''},
+        path='/api/lol/${region}/v${version}/summoner/${summonerIds}${filter}',
+    }
+
+    for index,summonerId in ipairs(ids) do
+        local cacheKeyFn = filter and cacheKeyForFilter[filter] or cacheKeyForId
+        local data = cache:get(cacheKeyFn(summonerId))
+
+        if data then
+            local key = filter and summonerId or self.standardizeSummonerName(data.name)
+            cachedData[key] = filter and cacheKeyFn == cacheKeyForId and data.name or data
+            cachedCount = cachedCount + 1
+        else
+            local idString = url.params.summonerIds
+            url.params.summonerIds = idString and idString..','..summonerId or summonerId
+
+            if (index - cachedCount) % maxNamesPerQuery == 0 then
+                self.api:get(url, onFilterResponse[filter] or onResponse)
+                url.params.summonerIds = nil
+            end
+        end
+    end
+
+    if url.params.summonerIds then
+        self.api:get(url, onFilterResponse[filter] or onResponse)
+    end
+
+    if cachedCount > 0 and callback then
+        callback(cachedData)
     end
 end
 
